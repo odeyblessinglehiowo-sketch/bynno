dc359dc142cb9e37b6c8717b6bb69cfa408efd42
Odey Blessing Lehiowo <odeyblessinglehiowo@gmail.com>
2026-05-25 15:09:20 +0100

    reg page

--- components/MobileStickyHeader.tsx
+++ components/MobileStickyHeader.tsx
@@ -78,7 +78,7 @@ export default function MobileStickyHeader() {
         </header>
 
         
-          <div className="h-[1px] w-full bg-[#FE001A]/50" />
+          
         </div>
       </div>
    
